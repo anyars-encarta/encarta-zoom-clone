e7af93b8879a71424f40657076f7072580a4e281
anyars-encarta <anyarsencarta@gmail.com>
2024-04-22 10:57:31 +0000

    Add current date and time

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,9 @@ const config = {
         blue: {
           1: '#0E78F9',
         },
+        sky: {
+          1: '#C9DDFF'
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -64,6 +67,9 @@ const config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      backgroundImage: {
+        hero: "url('/images/hero-background.png')"
       },
       keyframes: {
         "accordion-down": {
