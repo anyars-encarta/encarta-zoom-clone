08309432584bb6cc97479f12bf4f2cd3f795461e
anyars-encarta <anyarsencarta@gmail.com>
2024-04-22 18:49:47 +0000

    Add meetingh modal

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -7,6 +7,9 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
 
 interface MeetingModalProps {
     isOpen: boolean;
@@ -22,19 +25,29 @@ interface MeetingModalProps {
 
 const MeetingModal = ({ isOpen, onClose, title, className, children, handleClick, buttonText, image, buttonIcon }: MeetingModalProps) => {
     return (
-        <Dialog>
+        <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogTrigger>Open</DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
-                    </DialogDescription>
-                </DialogHeader>
+            <DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6py-9 text-white'>
+                <div className='flex flex-col gap-6'>
+                    {image && (
+                        <div className='flex justify-center'>
+                            <Image src={image} alt='image' width={72} height={72} />
+                        </div>
+                    )}
+                    <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>{title}</h1>
+                    {children}
+                    <Button
+                        className='bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0'
+                        onClick={handleClick}
+                    >
+                        {buttonIcon && (
+                            <Image src={buttonIcon} alt='buttin icon' width={13} height={13} />
+                        )} &nbsp;
+                        {buttonText || 'Schedule Meeting'}
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
-
     )
 }
 
