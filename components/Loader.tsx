017e981c5208aa8e0fe1e9b4d1ba65b14723c7f4
anyars-encarta <anyarsencarta@gmail.com>
2024-04-23 10:13:57 +0000

    Correct Loader styling

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
-import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='flex-cennter h-screen w-full'>
+    <div className='flex-center h-screen w-full'>
         <Image src='/icons/loading-circle.svg' alt='loading' width={50} height={50} />
     </div>
   )
