ba7e627b487564861464863189b4480fbcb95442
anyars-encarta <anyarsencarta@gmail.com>
2024-04-25 19:27:26 +0000

    Complete Join Meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useToast } from "@/components/ui/use-toast";
 import { Textarea } from "@/components/ui/textarea";
 import ReactDatePicker from 'react-datepicker';
+import { Input } from './ui/input';
 
 const MeetingTypeList = () => {
     const router = useRouter();
@@ -168,9 +169,11 @@ const MeetingTypeList = () => {
                 buttonText='Join Meeting'
                 handleClick={() => router.push(values.link)}
             >
-                
+                <Input placeholder='Meeting link'
+                className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+                onChange={(e) => setValues({ ...values, link: e.target.value})}
+                />
             </MeetingModal>
-
         </section>
     )
 }
