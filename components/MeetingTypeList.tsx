14d781879227e8ca3757501b7b30a866034cb492
anyars-encarta <anyarsencarta@gmail.com>
2024-04-25 12:19:41 +0000

    Implement schedule new meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -66,7 +66,8 @@ const MeetingTypeList = () => {
             toast({ title: "Failed to create meeting", })
         }
     }
-
+    
+    const meetingLink =`${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
             <HomeCard
@@ -142,8 +143,8 @@ const MeetingTypeList = () => {
                     title='Meeting Created'
                     className='text-center'
                     handleClick={() => {
-                        // navigator.clipboard.writeText(meetingLink);
-                        // toast({ title: 'Link Copied' })
+                        navigator.clipboard.writeText(meetingLink);
+                        toast({ title: 'Link Copied' })
                     }}
                     image='/icons/checked.svg'
                     buttonIcon='/icons/copy.svg'
