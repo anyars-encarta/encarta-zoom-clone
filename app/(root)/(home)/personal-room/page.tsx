0e49e40b827f7f1a286dd129c464b20552ecc5de
anyars-encarta <anyarsencarta@gmail.com>
2024-04-25 17:53:44 +0000

    Complete Personal Meeting Room

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -2,7 +2,10 @@
 
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/components/ui/use-toast';
+import { useGetCallById } from '@/hooks/useGetCallById';
 import { useUser } from '@clerk/nextjs';
+import { useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Table = ({ title, description }: { title: string; description: string }) => (
@@ -15,12 +18,28 @@ const Table = ({ title, description }: { title: string; description: string }) =
 const PersonalRoom = () => {
     const { user } = useUser();
     const meetingId = user?.id;
-    const meetingLink =`${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`
+    const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`
     const { toast } = useToast();
+    const client = useStreamVideoClient();
+    const router = useRouter();
+    
+    const { call } = useGetCallById(meetingId!);
 
     const startRoom = async () => {
+        if (!client || !user) return;
 
-    };
+        if (!call) {
+            const newCall = client.call('default', meetingId!)
+
+            await newCall.getOrCreate({
+                data: {
+                    starts_at: new Date().toISOString(),
+                }
+            })
+        }
+
+        router.push(`/meeting/${meetingId}?personal=true`)
+    }
 
     return (
         <section className='flex size-full flex-col gap-10 text-white'>
